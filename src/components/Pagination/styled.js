e178af92d35124af8ed6460b31893ce1715ae957
luanpires94 <luan.94pires@gmail.com>
2022-02-01 20:37:38 -0300

    feature pagination

---
+++ src/components/Pagination/styled.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const PaginationWrapper = styled.section`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    border-top: 1px solid #38444d;
+    color: #8899a6;
+    padding: 1.5rem 3rem;
+
+    a {
+        color: #8899a6;
+        text-align: none;
+        transition: color 0.5s;
+
+        &:hover {
+            color: #1fa1f2;
+        }
+    }
+`
